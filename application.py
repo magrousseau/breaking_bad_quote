59c7188377a806a97874c2c1e51900bea3c5a033
Magali Rousseau <rousseaumagali@gmail.com>
2023-04-25 15:51:46 +0400

    Add some basic HTML structure with minimal design

--- application.py
+++ application.py
@@ -1,22 +1,20 @@
+from flask import Flask, jsonify, render_template
 from flask import Flask, jsonify
 from quote import breaking_quote
 
 application = Flask(__name__)
 
 
-#homepage endpoint
 @application.route("/")
 def index():
-    return "<h1> Welcome to the home page </h1>"
+    return render_template('index.html')
 
 
-#quote endpoint
 @application.route("/quote")
 def quote():
-  random_quote = breaking_quote()
-  return jsonify({"quote": random_quote})
+    random_quote = breaking_quote()
+    return jsonify({"quote": random_quote})
 
 
-#this part is executed when the module is called directly - "pipenv run python application.py"
 if __name__ == "__main__":
-    application.run(debug=True)
+    application.run(debug=True).run(debug=True)
